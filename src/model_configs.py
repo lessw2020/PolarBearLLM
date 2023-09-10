7cbdbeacb6022163b0aa030300bc23d38b5741b0
Less Wright <lessw@etrillium.com>
2023-09-10 14:09:58 -0700

    Create model_configs.py

---
+++ src/model_configs.py
@@ -0,0 +1,27 @@
+import math
+import os
+import re
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, List, Tuple
+
+import torch
+import torch.nn as nn
+
+@dataclass
+class PolarBearConfig:
+  vocab_size: int = 50_304  
+  emb_dim: int = 768
+  num_heads: int = 2
+  num_kv_heads: int = 0
+  num_layers: int = 2
+  mlp_expansion_factor: float = 4
+  multiple_of: int = 256
+  activation_fn: str = "gelu"
+  p_dropout: float = 0.0
+  max_seq_len: int = 1024
+  pos_embeddings: str = "alibi"
+  use_flash: bool = True
+  use_triton_flash: bool = False
+  use_learned_emb: bool = True
+
